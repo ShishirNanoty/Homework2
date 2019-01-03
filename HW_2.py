b0307fbf88c48c4040184c9d591bf2da2768e5c4
ShishirN <nanotyshishir@gmail.com>
2019-01-03 19:00:55 +0530

    Code updated

--- HW_2.py
+++ HW_2.py
@@ -1,1 +1,23 @@
 #Homework_2
+# Metada for my favorite song with functions
+"""
+This file contains metadata stored in
+variables for a particular song
+"""
+def song():
+    song = 'Girls Like You' #name of song
+    print('Song: ', song)
+# song = 'Girls Like You' #name of song
+def rel():
+    rel = 'May 30, 2018' #Release date
+    print('Release: ',rel)
+def genre():
+    Genre = 'Pop'
+    print('Genre: ',Genre)
+def bln():
+    x = True
+    return x
+song()
+rel()
+genre()
+print('boolean variable is',bln())
